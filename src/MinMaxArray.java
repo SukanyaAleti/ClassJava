dca8b72a1da587365891e596dc9c373ca1806f4e
SukanyaAleti <sukanyaaleti2016@gmail.com>
2023-06-07 12:00:03 -0500

    Assignment 2 Added

---
+++ src/MinMaxArray.java
@@ -0,0 +1,21 @@
+public class MinMaxArray {
+        public static void main(String[] args) {
+            int[] numbers = {5, 10, 2, 15, 7};
+
+            int smallest = numbers[0];
+            int largest = numbers[0];
+
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] < smallest) {
+                    smallest = numbers[i];
+                }
+                if (numbers[i] > largest) {
+                    largest = numbers[i];
+                }
+            }
+
+            System.out.println("Smallest number: " + smallest);
+            System.out.println("Largest number: " + largest);
+        }
+    }
+
---
+++ src/ReverseArray.java
@@ -0,0 +1,20 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int n = numbers.length;
+        int start = 0, end = n - 1;
+
+        while (start < end) {
+            int temp = numbers[start];
+            numbers[start] = numbers[end];
+            numbers[end] = temp;
+            start++;
+            end--;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
