755f1ad71cd6452ebadbbc9d2791af25d37e8c94
SukanyaAleti <sukanyaaleti2016@gmail.com>
2023-06-09 15:35:20 -0500

    File Added

--- src/PolymorphismExample.java
+++ src/PolymorphismExample.java
@@ -12,7 +12,7 @@ class Car extends Vehicle {
 }
 
 public class PolymorphismExample {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Vehicle v1 = new Vehicle();
         Car c1 = new Car();
 
