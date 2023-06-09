821e3570f886af3edc21a3fa974edcc36df31403
SukanyaAleti <sukanyaaleti2016@gmail.com>
2023-06-09 15:24:49 -0500

    File Added

--- src/DepartmentTest.java
+++ src/DepartmentTest.java
@@ -2,9 +2,9 @@ abstract class Department{
     abstract int getDepartmentSize();
 }
 class CseDepartment extends Department{
-    private int size;
-    public CseDepartment(int size) {
-        this.size = size;
+     private int size;
+     public CseDepartment(int size) {
+          this.size = size;
     }
         public int getDepartmentSize() {
             return size;
@@ -12,7 +12,7 @@ class CseDepartment extends Department{
 }
 public class DepartmentTest {
     public static void main(String[] args) {
-        CseDepartment d = new CseDepartment(10);
+         CseDepartment d = new CseDepartment(10);
         int departmentSize = d.getDepartmentSize();
         System.out.println("Cse Department Size: " + departmentSize);
     }
