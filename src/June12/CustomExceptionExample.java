029f517f299fdae648a99bb50f031015db08c9f2
SukanyaAleti <sukanyaaleti2016@gmail.com>
2023-06-13 15:58:56 -0500

    File Added

--- src/June12/CustomExceptionExample.java
+++ src/June12/CustomExceptionExample.java
@@ -8,14 +8,12 @@ class CustomException extends Exception {
 public class CustomExceptionExample {
     public static void main(String[] args) {
         try {
-            // Some code that throws a runtime exception
             int[] numbers = {1, 2, 3};
             int index = 3;
             int value = numbers[index];
             System.out.println("value :" +value);
         } catch (ArrayIndexOutOfBoundsException e) {
             try {
-                // Convert the runtime exception into custom exception
                 throw new CustomException("Custom Exception: Invalid index");
             } catch (CustomException customException) {
                 System.out.println(customException.getMessage());
